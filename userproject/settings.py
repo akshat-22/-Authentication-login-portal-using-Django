41cd8275634d9c49151e13873378c21006df17ed
Akshat <jainakshatjain99@gmail.com>
2021-04-13 21:54:33 +0530

    Added more files

--- userproject/settings.py
+++ userproject/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-STATIC_ROOT="BASE_DIR /staticfiles"
+STATIC_ROOT = "/home/djangoprojects/myproject/static/"
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
